f2eeb6b9e9e6ab080d2cafdd1e3d75ca9a3c45ac
Owen Burns <owenburns88@gmail.com>
2022-09-21 22:09:52 -0400

    fixed experiment setup

--- experiment_setup.sh
+++ experiment_setup.sh
@@ -6,21 +6,58 @@ gpu=$3
 
 if [ $base_model == "MINERVA" ]
 then
-    cd src/MINERVA_tf2/datasets/data_preprocessed
+    cd src/MINERVA/datasets/data_preprocessed
     export PYTHONPATH=`pwd`
     echo $PYTHONPATH
     
-    if [ ! -f "fb60k.tgz"]
+    if [ ! -f "fb60k.tgz" ]
     then
         cat fb60k.tgz.parta* > fb60k.tgz
         rm -rf fb60k.tgz.parta*
     fi
 
-    [ $dataset == "FB15K-237" ] && [ ! -d "/FB15K-237" ] && tar -C /FB15K-237 -xvzf fb15k.tgz || echo "experiment $base_model-$dataset already prepared"
-    [ $dataset == "FB60K-NYT10" ] && [ ! -d "/FB60K-NYT10" ] && tar -C /FB60K-NYT10 -xvzf fb60k.tgz || echo "experiment $base_model-$dataset already prepared"
-    [ $dataset == "NELL-995" ] && [ ! -d "/nell-995" ] && tar -C /nell-995 -xvzf nell995.tgz || echo "experiment $base_model-$dataset already prepared"
-    [ $dataset == "WN18RR" ] && [ ! -d "/WN18RR" ] && tar -C /WN18RR -xvzf wn18rr.tgz || echo "experiment $base_model-$dataset already prepared"
-elif [ $base_model == "MultiHopKG-ConvE"]
+    if [ $dataset == "FB15K-237" ]
+    then
+        if [ ! -d "./FB15K-237" ]
+        then
+            mkdir "./FB15K-237"
+            tar -C ./FB15K-237 -xvzf fb15k.tgz
+        else
+            echo "experiment $base_model-$dataset already prepared"
+            exit 1
+        fi
+    elif [ $dataset == "FB60K-NYT10" ]
+    then
+        if [ ! -d "./FB60K-NYT10" ]
+        then
+            mkdir "./FB60K-NYT10"
+            tar -C ./FB60K-NYT10 -xvzf fb60k.tgz
+        else
+            echo "experiment $base_model-$dataset already prepared"
+            exit 1
+        fi
+    elif [ $dataset == "NELL-995" ]
+    then
+        if [ ! -d "./nell-995" ]
+        then
+            mkdir "./nell-995"
+            tar -C ./nell-995 -xvzf nell995.tgz
+        else
+            echo "experiment $base_model-$dataset already prepared"
+            exit 1
+        fi
+    elif [ $dataset == "WN18RR" ]
+    then
+        if [ ! -d "./WN18RR" ]
+        then
+            mkdir "./WN18RR"
+            tar -C ./WN18RR -xvzf wn18rr.tgz
+        else
+            echo "experiment $base_model-$dataset already prepared"
+            exit 1
+        fi
+    fi
+elif [ $base_model == "MultiHopKG-ConvE" ]
 then
     NELLFLAG=""
     if [ $dataset == "NELL-995" ]
@@ -28,15 +65,20 @@ then
         NELLFLAG="--test"
     fi
 
-    cd src/MultiHopKG
+    cd src/ConvE
     export PYTHONPATH=`pwd`
     echo $PYTHONPATH
 
-    [ ! -d "/data" ] && tar xvzf data-release.tgz
+    if [ ! -d "./data" ]
+    then
+        mkdir "./data"
+        tar -C ./data -xvzf data-release.tgz
+    fi
+
     echo "Processing $dataset"
-    ./experiment.sh configs/$dataset.sh --process_data $gpu $NELLFLAG
+    ./experiment.sh ../../configs/MultiHopKG-ConvE/$dataset.sh --process_data $gpu $NELLFLAG
     echo "Preparing ConvE model for $dataset"
-    ./experiment-emb.sh configs/$dataset-conve.sh --train $gpu $NELLFLAG
+    ./experiment-emb.sh ../../configs/MultiHopKG-ConvE/$dataset-conve.sh --train $gpu $NELLFLAG
 else
     echo Invalid RL base model specified; taking no action
 fi
